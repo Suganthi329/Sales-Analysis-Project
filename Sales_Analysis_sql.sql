6ed723f5b4f14ea6e6c1f9f82a2a5e0eb9768e7b
Suganthi329 <suganthikuzhanthaivel@gmail.com>
2026-06-20 13:06:14 +0530

    Update Sales_Analysis_sql.sql

--- Sales_Analysis_sql.sql
+++ Sales_Analysis_sql.sql
@@ -1,5 +1,6 @@
 use	salesDB;
-//Create  sales Table//
+
+/*Create  sales Table*/
 CREATE TABLE Sales (
     Order_ID VARCHAR(20),
     Amount INT,
@@ -13,96 +14,113 @@ CREATE TABLE Sales (
     State VARCHAR(50),
     City VARCHAR(50)
 );
-//Insert values  into the table//
+
+/*Insert values  into the table*/
 INSERT INTO Sales
 VALUES
 ('B-26776', 9726, 1275, 5, 'Electronics', 'Phones',
  'UPI', '2022-01-01', 'John Smith', 'California', 'Los Angeles');
-//View Data//
+
+/*View Data*/
 select * from Sales;
-//Total Sales//
+
+/*Total Sales*/
 SELECT SUM(Amount) AS TotalSales
 FROM Sales;
-//Total Profit//
+
+/*Total Profit*/
 SELECT SUM(Profit) AS TotalProfit
 FROM Sales;
-//Total Orders//
+
+/*Total Orders*/
 SELECT COUNT(DISTINCT Order_ID) AS TotalOrders
 FROM Sales;
-//Total Quantity//
+
+/*Total Quantity*/
 SELECT SUM(Quantity) AS TotalQuantity
 FROM Sales;
-//Sales by category//
+
+/*Sales by category*/
 SELECT
 Category,
 SUM(Amount) AS Sales
 FROM Sales
 GROUP BY Category
 ORDER BY Sales DESC;
-//Profit by category//
+
+/*Profit by category*/
 SELECT
 Category,
 SUM(Profit) AS Profit
 FROM Sales
 GROUP BY Category
 ORDER BY Profit DESC;
-//Sales by subcategory//
+
+/*Sales by subcategory*/
 SELECT
 Sub_Category,
 SUM(Amount) AS Sales
 FROM Sales
 GROUP BY Sub_Category
 ORDER BY Sales DESC;
-//Profit by subcategory//
+
+/*Profit by subcategory*/
 SELECT
 Sub_Category,
 SUM(Profit) AS Profit
 FROM Sales
 GROUP BY Sub_Category
 ORDER BY Profit DESC;
-//Top 10 customer by sales//
+
+/*Top 10 customer by sales*/
 SELECT TOP 10
 CustomerName,
 SUM(Amount) AS Sales
 FROM Sales
 GROUP BY CustomerName
 ORDER BY Sales DESC;
-//state wise sales//
+
+/*state wise sales*/
 SELECT
 State,
 SUM(Amount) AS Sales
 FROM Sales
 GROUP BY State
 ORDER BY Sales DESC;
-//State wise profit//
+
+/*State wise profit*/
 SELECT
 State,
 SUM(Profit) AS Profit
 FROM Sales
 GROUP BY State
 ORDER BY Profit DESC;
-//city wise sales//
+
+/*city wise sales*/
 SELECT
 City,
 SUM(Amount) AS Sales
 FROM Sales
 GROUP BY City
 ORDER BY Sales DESC;
-//Payment mode analysis//
+
+/*Payment mode analysis*/
 SELECT
 PaymentMode,
 COUNT(*) AS Orders
 FROM Sales
 GROUP BY PaymentMode
 ORDER BY Orders DESC;
-//Monthly sales trend//
+
+/*Monthly sales trend*/
 SELECT
 FORMAT(Order_Date, 'yyyy-MM') AS YearMonth,
 SUM(Amount) AS Sales
 FROM Sales
 GROUP BY FORMAT(Order_Date, 'yyyy-MM')
 ORDER BY YearMonth;
-//Profit Margin//
+
+/*Profit Margin*/
 SELECT
 ROUND(
     SUM(Profit) * 100.0 / SUM(Amount),
